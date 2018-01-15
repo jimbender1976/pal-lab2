ebe75eee90e7549bae2328b4d4e8f3d38c97959c
Steve Reichert <steve.reichert@ex.kroger.com>
2018-01-15 14:30:31 -0700

    Added Welcome and Env Controller

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,26 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private Map<String, String> envMap = new HashMap<>();
+
+    public EnvController(@Value("${PORT:NOT SET}") String port, @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit, @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex, @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddr) {
+        envMap.put("PORT", port);
+        envMap.put("MEMORY_LIMIT", memoryLimit);
+        envMap.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        envMap.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() throws Exception {
+        return envMap;
+    }
+}
