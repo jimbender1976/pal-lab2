d64086ce054ae58ebb6209b3180e1600be68ee2e
Brennan Blackwell <Brennan.Blackwell@kroger.com>
2018-01-16 10:49:36 -0500

    updated manifest.yml for review and production

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,25 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    @Autowired
+    public WelcomeController( @Value("${WELCOME_MESSAGE}") String helloMessage) {
+        this.helloMessage = helloMessage;
+    }
+
+
+    private String helloMessage;
+
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        //return "hello";
+        return helloMessage;
     }
 }
